2420fb6c425d5eb2dee10ca3de266d734dcf004c
sarafashka <96177099+sarafashka@users.noreply.github.com>
2022-07-26 14:19:50 +0300

    solving part5 promises

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -133,8 +133,14 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const down1 = rect1.top + rect1.height;
+  const right1 = rect1.left + rect1.width;
+
+  const top2 = rect2.top;
+  const left2 = rect2.left;
+
+  return left2 <= right1 && top2 <= down1;
 }
 
 
